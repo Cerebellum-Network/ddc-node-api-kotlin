f5998b37885e5e9ed83488e64b354cfaec3aa4f2
Sergey Poluyan <spoluyan@gmail.com>
2021-03-09 18:11:04 +0300

    Fix metrics request

--- src/main/kotlin/network/cere/ddc/node/api/metrics/GetMetricsRequest.kt
+++ src/main/kotlin/network/cere/ddc/node/api/metrics/GetMetricsRequest.kt
@@ -3,6 +3,7 @@ package network.cere.ddc.node.api.metrics
 import java.time.Instant
 
 data class GetMetricsRequest(
+    val nodeAddress: String,
     val appPubKey: String? = null,
     val partitionId: String? = null,
     val from: Instant? = null,
